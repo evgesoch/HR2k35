1cc731f73d485847eb9e019865a870e35216b801
liantzakis-it <liantzakis.it@gmail.com>
2019-02-12 23:43:53 +0200

    Removed unnecessary comments

--- Datasource.java
+++ Datasource.java
@@ -6,7 +6,6 @@ public class Datasource {
 
     private static final String CONNECTION_STATEMENT = "jdbc:sqlite:" + DB_NAME;
 
-    //could change visibility to public if these constants are needed in other classes
     private static final String TABLE_CANDIDATES = "candidates";
     private static final String COLUMN_ID = "id";
     private static final String COLUMN_NAME = "name";

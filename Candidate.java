a3a1ed729df6546ac800a4fdda76235acef43fab
vlzvl <burzum_mewtwo@yahoo.gr>
2019-02-12 23:47:57 +0200

    Update Candidate.java
    Update the JDoc descriptions for Candidate.java

--- Candidate.java
+++ Candidate.java
@@ -32,58 +32,114 @@ public class Candidate {
         this.city = city;
     }
 
+	/**
+     * Returns the id of a candidate.
+     *
+     * @return The id as an {@code int}
+     */
     public int getId() {
         return id;
     }
 
+	/**
+     * Returns the name of a candidate.
+     *
+     * @return The name as a {@code String}
+     */
     public String getName() {
         return name;
     }
 
+	/**
+     * Returns the e-mail of a candidate.
+     *
+     * @return The e-mail as a {@code String}
+     */
     public String getEmail() {
         return email;
     }
 
+	/**
+     * Returns the telephone of a candidate.
+     *
+     * @return The telephone as a {@code String}
+     */
     public String getPhone() {
         return phone;
     }
 
+	/**
+     * Returns the date of birth of a candidate.
+     *
+     * @return The date of birth as a {@code String}
+     */
     public String getDob() {
         return dob;
     }
 
+	/**
+     * Returns the profession of a candidate.
+     *
+     * @return The profession as a {@code String}
+     */
     public String getProfession() {
         return profession;
     }
 
+	/**
+     * Returns the city of a candidate.
+     *
+     * @return The city as a {@code String}
+     */
     public String getCity() {
         return city;
     }
 
+	/**
+     * Sets the id of a candidate.
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+	/**
+     * Sets the name of a candidate.
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+	/**
+     * Sets the e-mail of a candidate.
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+	/**
+     * Sets the telephone of a candidate.
+     */
     public void setPhone(String phone) {
         this.phone = phone;
     }
 
+	/**
+     * Sets the date of birth of a candidate.
+     */
     public void setDob(String dob) {
         this.dob = dob;
     }
 
+	/**
+     * Sets the profession of a candidate.
+     */
     public void setProfession(String profession) {
         this.profession = profession;
     }
 
+	/**
+     * Sets the city of a candidate.
+     */
     public void setCity(String city) {
         this.city = city;
     }
